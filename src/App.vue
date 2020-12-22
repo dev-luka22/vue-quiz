88573a1a0e30a73f99f9a78879a9503b83ed5394
dev-luka22 <dev.luka22@gmail.com>
2020-12-22 21:46:34 +0100

    Cleaned App and HW Component

--- src/App.vue
+++ src/App.vue
@@ -15,4 +15,20 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  max-width: 1920px;
+  margin: 0 auto;
+}
+
+#app {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+}
+</style>
