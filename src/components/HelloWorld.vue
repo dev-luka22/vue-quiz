a6e629bfa31277fb1f7c0e2916757e62e65dd1a3
dev-luka22 <dev.luka22@gmail.com>
2021-01-04 16:06:23 +0100

    Added color change logic

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,104 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <div v-if="currentQuestion < quizData.length">
+      <Questions
+        :question="quizData[currentQuestion].question"
+        :first="quizData[currentQuestion].a"
+        :second="quizData[currentQuestion].b"
+        :third="quizData[currentQuestion].c"
+        @actionFoo="actionFoo"
+        ref="Child"
+      />
+      <button
+        @click="newQuestion"
+        v-on:click="$refs.Child.selectItem()"
+        class="button-next"
+      >
+        Next
+      </button>
+    </div>
+
+    <div v-if="currentQuestion >= quizData.length">
+      <p>You scored {{ trueAnswers }}/{{ quizData.length }}</p>
+      <button @click="restart">Restart</button>
+    </div>
+  </div>
 </template>
 
 <script>
+import Questions from "./Questions";
+
 export default {
   name: "HelloWorld",
+  components: {
+    Questions,
+  },
+  data() {
+    return {
+      currentQuestion: 0,
+      trueAnswers: 0,
+      legal: false,
+      quizData: [
+        {
+          question: "First?",
+          a: "false",
+          b: "false",
+          c: "true",
+          correct: "c",
+        },
+        {
+          question: "Second?",
+          a: "false",
+          b: "true",
+          c: "false",
+          correct: "b",
+        },
+        {
+          question: "Third?",
+          a: "true",
+          b: "false",
+          c: "false",
+          correct: "a",
+        },
+      ],
+    };
+  },
+  methods: {
+    newQuestion() {
+      if (this.legal === true) {
+        this.trueAnswers++;
+        this.legal = false;
+        this.currentQuestion++;
+      } else {
+        this.legal = false;
+        this.currentQuestion++;
+      }
+    },
+    actionFoo(e) {
+      if (e === this.quizData[this.currentQuestion].correct) {
+        this.legal = true;
+      } else if (e !== this.quizData[this.currentQuestion].correct) {
+        this.legal = false;
+      }
+    },
+    restart() {
+      this.currentQuestion = 0;
+      this.trueAnswers = 0;
+    },
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  width: 100%;
+}
+
+.container div {
+  width: 100%;
+}
+.button-next {
+  display: flex;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/Questions.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="container">
+    <div class="holder">
+      <p>Question: {{ question }}</p>
+      <p
+        @click="$emit('actionFoo', 'a')"
+        v-on:click="selectItem"
+        class="changeColorArr"
+      >
+        a: {{ first }}
+      </p>
+      <p
+        @click="$emit('actionFoo', 'b')"
+        class="changeColorArr"
+        v-on:click="selectItem"
+      >
+        b: {{ second }}
+      </p>
+      <p
+        @click="$emit('actionFoo', 'c')"
+        class="changeColorArr"
+        v-on:click="selectItem"
+      >
+        c: {{ third }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Questions",
+  props: {
+    question: null,
+    first: null,
+    second: null,
+    third: null,
+  },
+  methods: {
+    selectItem(el) {
+      const changeColorArr = document.querySelectorAll(".changeColorArr");
+      changeColorArr.forEach((e) => {
+        e.style.backgroundColor = "cornsilk";
+      });
+      if (el) {
+        el.currentTarget.style.backgroundColor = "green";
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+
+.holder {
+  width: 500px;
+}
+
+p {
+  padding: 10px;
+  background-color: cornsilk;
+  margin-bottom: 10px;
+  cursor: pointer;
+}
+</style>
